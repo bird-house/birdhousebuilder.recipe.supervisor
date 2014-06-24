d66840eba4d174d8ee2f0b1b1579fb669ff89f7a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-06-24 13:03:02 +0200

    mkdirs for supervisor config

--- birdhousebuilder/recipe/supervisor/__init__.py
+++ birdhousebuilder/recipe/supervisor/__init__.py
@@ -26,16 +26,20 @@ class Supervisor(object):
         self.buildout, self.name, self.options = buildout, name, options
         b_options = buildout['buildout']
         self.anaconda_home = b_options.get('anaconda-home', '/opt/anaconda')
+        bin_path = os.path.join(self.anaconda_home, 'bin')
+        lib_path = os.path.join(self.anaconda_home, 'lib')
         self.conda_channels = b_options.get('conda-channels')
 
         self.program = options.get('program', name)
         self.command = options.get('command')
-        self.directory =  options.get('directory', '${buildout:anaconda-home}/bin')
+        self.directory =  options.get('directory', bin_path)
         self.priority = options.get('priority', '10')
-        self.environment = options.get('environment', 'PATH="${buildout:anaconda-home}/bin",LD_LIBRARY_PATH="${buildout:anaconda-home}/lib"')
+        self.environment = options.get('environment',
+                                       'PATH="%s",LD_LIBRARY_PATH="%s"' % (bin_path, lib_path))
 
     def install(self):
-        """run the commands
+        """
+        install supervisor config file from template
         """
         result = mytemplate.render(
             program=self.program,
@@ -44,10 +48,20 @@ class Supervisor(object):
             priority=self.priority,
             environment=self.environment)
 
-        output = os.path.join(self.anaconda_home, 'etc', 'supervisor', 'conf.d', self.program)
+        output = os.path.join(self.anaconda_home, 'etc', 'supervisor', 'conf.d', self.program + '.conf')
+        try:
+            os.makedirs(os.path.dirname(output))
+        except OSError:
+            pass
+        
+        try:
+            os.remove(output)
+        except OSError:
+            pass
+
         with open(output, 'wt') as fp:
             fp.write(result)
-        return self.options.created()
+        return self.options.created(output)
 
     def update(self):
         return self.install()
