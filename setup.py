044138c916f539a03e10e44e5bd6007aef2c3b0f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-12 17:51:46 +0100

    update version 0.3.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 name = 'birdhousebuilder.recipe.supervisor'
 
-version = '0.3.4'
+version = '0.3.5'
 description = 'A Buildout recipe to install and configure supervisor for Anaconda.'
 long_description = (
     open('README.rst').read() + '\n' +
